e1040918af4c12e12dddfa1c50f7f5dd19e30ae1
alexis jimenez castillo <alexis.jcastillo@outlook.com>
2023-06-29 13:56:55 +0200

    second commit

--- Andrey Navas/src/App.jsx
+++ Andrey Navas/src/App.jsx
@@ -1,11 +1,19 @@
-import React from "react";
+import { Box, Container } from "@mui/material";
 import Button from "@mui/material/Button";
 
 export default function App() {
   return (
-    <>
+    <Container>
       <h1>App</h1>
+      <Box sx={{
+        border: "5px",
+        p:8 ,
+        borderStyle: "solid",
+        boxShadow: 3,
+        bgcolor: "#111",
+        }}>
+      pero que belleza</Box>
       <Button variant="contained">El botón</Button>
-    </>
+    </Container>
   );
 }
